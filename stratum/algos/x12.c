5423a1c607c57ab45c2e2830b6e09e0806e31db4
0xEAX <51427116+0xEAX@users.noreply.github.com>
2019-06-13 18:37:09 -0400

    Fix include headers

--- stratum/algos/x12.c
+++ stratum/algos/x12.c
@@ -3,18 +3,18 @@
 #include <string.h>
 #include <stdio.h>
 
-#include <sha3/sph_blake.h>
-#include <sha3/sph_bmw.h>
-#include <sha3/sph_luffa.h>
-#include <sha3/sph_cubehash.h>
-#include <sha3/sph_shavite.h>
-#include <sha3/sph_simd.h>
-#include <sha3/sph_echo.h>
-#include <sha3/sph_groestl.h>
-#include <sha3/sph_skein.h>
-#include <sha3/sph_jh.h>
-#include <sha3/sph_keccak.h>
-#include <sha3/sph_hamsi.h>
+#include "../sha3/sph_blake.h"
+#include "../sha3/sph_bmw.h"
+#include "../sha3/sph_luffa.h"
+#include "../sha3/sph_cubehash.h"
+#include "../sha3/sph_shavite.h"
+#include "../sha3/sph_simd.h"
+#include "../sha3/sph_echo.h"
+#include "../sha3/sph_groestl.h"
+#include "../sha3/sph_skein.h"
+#include "../sha3/sph_jh.h"
+#include "../sha3/sph_keccak.h"
+#include "../sha3/sph_hamsi.h"
 
 void x12_hash(const char* input, char* output, uint32_t len)
 {
