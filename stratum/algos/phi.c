eea5b468d73770b2e5848120491193f66a206443
0xEAX <51427116+0xEAX@users.noreply.github.com>
2019-06-14 18:48:48 -0400

    Fix include headers

--- stratum/algos/phi.c
+++ stratum/algos/phi.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 
-#include <sha3/sph_skein.h>
-#include <sha3/sph_jh.h>
-#include <sha3/sph_cubehash.h>
-#include <sha3/sph_fugue.h>
-#include <sha3/sph_echo.h>
-#include "gost.h"
+#include "../sha3/sph_skein.h"
+#include "../sha3/sph_jh.h"
+#include "../sha3/sph_cubehash.h"
+#include "../sha3/sph_fugue.h"
+#include "../sha3/sph_echo.h"
 
+#include "gost.h"
 #include "common.h"
 
 void phi_hash(const char* input, char* output, uint32_t len)
