09aa1f4686cb3705f1c3995399e36365d76cfb46
0xEAX <51427116+0xEAX@users.noreply.github.com>
2019-06-14 19:34:53 -0400

    Fix include headers

--- stratum/algos/aergo.c
+++ stratum/algos/aergo.c
@@ -1,28 +1,28 @@
-#include "aergo.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 
-#include <sha3/sph_blake.h>
-#include <sha3/sph_bmw.h>
-#include <sha3/sph_groestl.h>
-#include <sha3/sph_jh.h>
-#include <sha3/sph_keccak.h>
-#include <sha3/sph_skein.h>
-#include <sha3/sph_luffa.h>
-#include <sha3/sph_cubehash.h>
-#include <sha3/sph_shavite.h>
-#include <sha3/sph_simd.h>
-#include <sha3/sph_echo.h>
-#include <sha3/sph_hamsi.h>
-#include <sha3/sph_fugue.h>
-#include <sha3/sph_shabal.h>
-#include <sha3/sph_whirlpool.h>
-#include <sha3/sph_haval.h>
-#include "gost.h"
+#include "../sha3/sph_blake.h"
+#include "../sha3/sph_bmw.h"
+#include "../sha3/sph_groestl.h"
+#include "../sha3/sph_jh.h"
+#include "../sha3/sph_keccak.h"
+#include "../sha3/sph_skein.h"
+#include "../sha3/sph_luffa.h"
+#include "../sha3/sph_cubehash.h"
+#include "../sha3/sph_shavite.h"
+#include "../sha3/sph_simd.h"
+#include "../sha3/sph_echo.h"
+#include "../sha3/sph_hamsi.h"
+#include "../sha3/sph_fugue.h"
+#include "../sha3/sph_shabal.h"
+#include "../sha3/sph_whirlpool.h"
+#include "../sha3/sph_haval.h"
 
+#include "aergo.h"
 #include "common.h"
+#include "gost.h"
 
 void aergo_hash(const char* input, char* output, uint32_t len)
 {
